ada8d07161c168f76bb4b1e8d4e46093dec440c7
Melvin Hillsman <mhillsma@redhat.com>
2026-05-23 14:53:37 -0500

    test(e2e): add TPS workload deployment and cleanup tests
    Verify that the tps workload deploys both server and client VMs, creates
    the three-port service (netperf-ctrl, netperf-data, http-data), and
    cleans up all resources. Covers the multi-VM deployment path that was
    previously untested end-to-end.
    
    Signed-off-by: Melvin Hillsman <mhillsma@redhat.com>

---
+++ tests/e2e/tps_test.go
@@ -0,0 +1,78 @@
+//go:build e2e
+
+// Copyright 2026 Red Hat
+// SPDX-License-Identifier: Apache-2.0
+
+package e2e_test
+
+import (
+	"context"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/opdev/virtwork/internal/testutil"
+	"github.com/opdev/virtwork/internal/vm"
+)
+
+var _ = Describe("TPS workload", Label("slow"), func() {
+	var namespace string
+
+	BeforeEach(func() {
+		namespace = testutil.UniqueNamespace("e2e-tps")
+	})
+
+	AfterEach(func() {
+		_, _, _, _ = testutil.RunVirtwork("cleanup",
+			"--namespace", namespace, "--delete-namespace")
+	})
+
+	It("should deploy server and client VMs with a service", func() {
+		stdout, _, exitCode, err := testutil.RunVirtwork(
+			"run", "--workloads", "tps", "--vm-count", "1",
+			"--namespace", namespace, "--no-wait")
+		Expect(err).NotTo(HaveOccurred())
+		Expect(exitCode).To(Equal(0))
+		Expect(stdout).To(ContainSubstring("virtwork-tps-server-0"))
+		Expect(stdout).To(ContainSubstring("virtwork-tps-client-0"))
+
+		c := testutil.MustConnect("")
+		ctx := context.Background()
+
+		svc := &corev1.Service{}
+		err = c.Get(ctx, client.ObjectKey{
+			Name: "virtwork-tps-server", Namespace: namespace,
+		}, svc)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(svc.Spec.Ports).To(HaveLen(3))
+
+		vms, err := vm.ListVMs(ctx, c, namespace, testutil.ManagedLabels())
+		Expect(err).NotTo(HaveOccurred())
+		Expect(vms).To(HaveLen(2))
+	})
+
+	It("should clean up all TPS resources", func() {
+		_, _, exitCode, err := testutil.RunVirtwork(
+			"run", "--workloads", "tps", "--vm-count", "1",
+			"--namespace", namespace, "--no-wait")
+		Expect(err).NotTo(HaveOccurred())
+		Expect(exitCode).To(Equal(0))
+
+		stdout, _, exitCode, err := testutil.RunVirtwork(
+			"cleanup", "--namespace", namespace)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(exitCode).To(Equal(0))
+		Expect(stdout).To(ContainSubstring(`"vms_deleted":2`))
+
+		c := testutil.MustConnect("")
+		ctx := context.Background()
+		Eventually(func() int {
+			vms, err := vm.ListVMs(ctx, c, namespace, testutil.ManagedLabels())
+			Expect(err).NotTo(HaveOccurred())
+			return len(vms)
+		}, 60*time.Second, 2*time.Second).Should(Equal(0))
+	})
+})
