26e74adc96846abb6bf3c08180c40f3355d14f97
Melvin Hillsman <mhillsma@redhat.com>
2026-05-28 15:12:07 -0500

    test(version): add failing tests for version subcommand
    Red phase: tests expect newVersionCmd to exist with output containing
    version, commit, and build date (showing defaults when not set via ldflags).
    
    Resolves #119
    
    Signed-off-by: Melvin Hillsman <mhillsma@redhat.com>

---
+++ cmd/virtwork/version_test.go
@@ -0,0 +1,50 @@
+// Copyright 2026 Red Hat
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"bytes"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestVersionSuite(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Version Suite")
+}
+
+var _ = Describe("Version command", func() {
+	It("should execute without error", func() {
+		cmd := newVersionCmd()
+		var buf bytes.Buffer
+		cmd.SetOut(&buf)
+		Expect(cmd.Execute()).To(Succeed())
+	})
+
+	It("should print virtwork in the output", func() {
+		cmd := newVersionCmd()
+		var buf bytes.Buffer
+		cmd.SetOut(&buf)
+		Expect(cmd.Execute()).To(Succeed())
+		Expect(buf.String()).To(ContainSubstring("virtwork"))
+	})
+
+	It("should show dev when version is not set via ldflags", func() {
+		cmd := newVersionCmd()
+		var buf bytes.Buffer
+		cmd.SetOut(&buf)
+		Expect(cmd.Execute()).To(Succeed())
+		Expect(buf.String()).To(ContainSubstring("(dev)"))
+	})
+
+	It("should show unknown when commit is not set via ldflags", func() {
+		cmd := newVersionCmd()
+		var buf bytes.Buffer
+		cmd.SetOut(&buf)
+		Expect(cmd.Execute()).To(Succeed())
+		Expect(buf.String()).To(ContainSubstring("(unknown)"))
+	})
+})
